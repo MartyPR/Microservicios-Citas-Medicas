bb47ab283f716b8314b0fe85be898cff05e1c17c
MartyPR <martin.p1131@gmail.com>
2024-09-25 09:51:45 -0500

    se define el contexto de base de datos y c ontrolador que gestiona las operaciones CRUD

---
+++ PersonasMicroservice/Controllers/PersonasController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PersonasMicroservice.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PersonasController : ControllerBase
+{
+    private readonly PersonasDbContext _context;
+
+    public PersonasController(PersonasDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Persona>>> GetPersonas()
+    {
+        return await _context.Personas.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Persona>> GetPersonaById(int id)
+    {
+        var persona = await _context.Personas.FindAsync(id);
+        if (persona == null)
+            return NotFound();
+        return persona;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Persona>> CreatePersona(Persona persona)
+    {
+        _context.Personas.Add(persona);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetPersonaById), new { id = persona.Id }, persona);
+    }
+}
---
+++ PersonasMicroservice/Data/PersonasContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonasMicroservice.Models;
+public class PersonasDbContext : DbContext
+{
+    public DbSet<Persona> Personas { get; set; }
+
+    public PersonasDbContext(DbContextOptions<PersonasDbContext> options) : base(options) { }
+}
